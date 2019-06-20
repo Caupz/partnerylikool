446b85c761db518345bf0640f47aac9d583233fa
Caupo Helvik <helvikcaupo@gmail.com>
2019-06-20 19:46:42 +0300

    Update index.php
    Final notes

--- web/index.php
+++ web/index.php
@@ -1,4 +1,17 @@
 <?php
+
+/*
+  Following project is built on self-made MVC platform. Lots of things like class or function names are inspired from Yii2 Framework.
+  We know that some things in this code are unnecessary or can be refactored, but we just don't have enough time to do that.
+
+  Architect, Lead Developer & Project manager: Caupo Helvik
+  Backend: Priit Laupa, Lauri Raus
+  Frontend: Kristjan Põldmets
+  Documentation: Rasmus Aron
+  
+  If you have any questions about the project then feel free to ask by sending me an email to helvikcaupo@gmail.com.
+*/
+
 require(__DIR__ . '/autoloader.php');
 use app\components\Router;
 
